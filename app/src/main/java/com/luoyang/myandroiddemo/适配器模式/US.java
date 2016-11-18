5049fe6643ee145c94715f4bee1618637b853953
luoyang <toluoyang@qq.com>
2016-11-18 14:10:10 +0800

    适配器模式

---
+++ app/src/main/java/com/luoyang/myandroiddemo/适配器模式/US.java
@@ -0,0 +1,11 @@
+package com.luoyang.myandroiddemo.适配器模式;
+
+/**
+ * Created by LuoYang on 2016/11/18.
+ */
+
+public class US {
+    public void sayUS(String s) {
+        System.out.println("hello," + s);
+    }
+}
--- app/src/test/java/com/luoyang/myandroiddemo/ExampleUnitTest.java
+++ app/src/test/java/com/luoyang/myandroiddemo/ExampleUnitTest.java
@@ -1,5 +1,9 @@
 package com.luoyang.myandroiddemo;
 
+import com.luoyang.myandroiddemo.适配器模式.CH;
+import com.luoyang.myandroiddemo.适配器模式.TransLator;
+import com.luoyang.myandroiddemo.适配器模式.US;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -13,5 +17,11 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() throws Exception {
         assertEquals(4, 2 + 2);
+    }
+    @Test
+    public void translatorTest() throws Exception{
+        US usMan = new US();
+        CH chMan = new TransLator(usMan);
+        chMan.sayCH("US的朋友");
     }
 }
